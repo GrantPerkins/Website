2cdf4c53aedb1ec9bfd1e40de9c65eb6e2b79e11
GrantPerkins <gcperk20@gmail.com>
2020-12-09 00:59:24 -0500

    Initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import {HashRouter, Route, Switch} from "react-router-dom";
+import {createMuiTheme, CssBaseline} from "@material-ui/core";
+import {ThemeProvider} from "@material-ui/styles";
+import Home from "../home/Home";
+
+const theme = createMuiTheme({
+    palette: {
+        background: {
+            default: "#30476b"
+        },
+        text: {
+            primary: "#30476b"
+        },
+        primary: "#30476b"
+    }
+});
+
+function App() {
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <HashRouter>
+                    <Switch>
+                        <Route path={"/"}>
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </HashRouter>
+            </ThemeProvider>
+        </div>
+);
+}
+
+export default App;
---
+++ src/components/home/Home.js
@@ -0,0 +1,84 @@
+import React, {Component} from "react";
+import {Card, CardContent, Collapse, Grid, IconButton, Typography} from "@material-ui/core";
+import {Email, GitHub, LinkedIn, Reddit, YouTube} from "@material-ui/icons";
+import pfp from "./images/pfp.jpg";
+import "./Home.css";
+
+
+const styles = {
+
+    largeIcon: {
+        width: 60,
+        height: 60,
+        color: "primary"
+    },
+    image: {
+        width: 250,
+        height: 250,
+        borderRadius: "50%",
+        border: "7px solid primary"
+    },
+    header: {
+        paddingTop: 10,
+    },
+
+
+};
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {collapsed: false};
+    }
+
+    componentDidMount() {
+        this.setState({collapsed: true});
+    }
+
+    render() {
+        const collapsed = this.state.collapsed;
+        return (
+            <div className={"home"}>
+                <Collapse in={collapsed} timeout={2000}>
+                    <div className={"content"}>
+                        <Card>
+                            <Typography variant={"h2"} style={styles.header}> Grant Perkins </Typography>
+                            <CardContent>
+                                <img src={pfp} alt={"me"} style={styles.image}/>
+                                <Grid container spacing={2} direction={"row"} justify={"center"}>
+                                    <Grid item>
+                                        <IconButton href={"https://github.com/GrantPerkins"}>
+                                            <GitHub style={styles.largeIcon}/>
+                                        </IconButton>
+                                    </Grid>
+                                    <Grid item>
+                                        <IconButton href={"https://www.reddit.com/user/TenseOrBored"}>
+                                            <Reddit style={styles.largeIcon}/>
+                                        </IconButton>
+                                    </Grid>
+                                    <Grid item>
+                                        <IconButton href={"https://www.linkedin.com/in/grant-perkins-35ba00170/"}>
+                                            <LinkedIn style={styles.largeIcon}/>
+                                        </IconButton>
+                                    </Grid>
+                                    <Grid item>
+                                        <IconButton href={"mailto:gcperkins@wpi.edu"}>
+                                            <Email style={styles.largeIcon}/>
+                                        </IconButton>
+                                    </Grid>
+                                    <Grid item>
+                                        <IconButton href={"https://www.youtube.com/channel/UCbBPHIVcsizYd1IPcwIbL5g"}>
+                                            <YouTube style={styles.largeIcon}/>
+                                        </IconButton>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </Collapse>
+            </div>
+        );
+    }
+}
+
+export default Home;
